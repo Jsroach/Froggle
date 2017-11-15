ff6e6242c0afa87941f64bf4e2939b9c075afefb
Jack <jsroach@uvm.edu>
2017-11-15 14:54:56 -0500

    Vowels

---
+++ Vowel.h
@@ -0,0 +1,26 @@
+//
+// Created by Jack Roach on 11/15/17.
+//
+#include "Piece.h""
+#ifndef MCJJ_VOWEL_H
+#define MCJJ_VOWEL_H
+
+#include <vector>
+using namespace std;
+
+class Vowel : public Piece{
+public:
+    Vowel();
+    Vowel(char c);
+    Vowel(char c, int x, int y);
+    Vowel(int x, int y);
+
+    char generate_vowel(){
+
+    }
+protected:
+    vector<char> letters = {'a','e','i','o','u','y'};
+};
+
+
+#endif //MCJJ_VOWEL_H
