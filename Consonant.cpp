67b2be367028349e23419dafaee1a53ad8d4b269
cniemczy <cniemczy@uvm.edu>
2017-11-16 17:38:48 -0500

    Fix small bugs in consonant

--- Consonant.cpp
+++ Consonant.cpp
@@ -2,6 +2,7 @@
 // Created by cn345 on 11/15/2017.
 //
 #include "Consonant.h"
+#include <iostream>
 
 Consonant::Consonant() = default {}
 
@@ -10,14 +11,14 @@ Consonant::Consonant(char characterIn) : setCharacter(characterIn) {}
 Consonant::Consonant(char characterIn, int xIn, int yIn) : setCharacter(characterIn), setX(xIn), setY(yIn) {}
 
 void Consonant::movePiece() {
-    if (y == SPACE && x > 0) {
-        setx(getX() - SPACE);
-    }else if (y == SPACE * 2 && x < SPACE * 50) {
-        setx(getX() + SPACE);
+    if (getY() == SPACE && getX() > 0) {
+        setX(getX() - SPACE);
+    }else if (getY() == SPACE * 2 && getX() < SPACE * 50) {
+        setX(getX() + SPACE);
     }
 }
 
-voide Consonant::setCharacter(char characterIn) {
+void Consonant::setCharacter(char characterIn) {
     bool check = false;
     for (int i = 0; i < cPieces.size(); i++) {
         if (characterIn == cPieces[i]) {
