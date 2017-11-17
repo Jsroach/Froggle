cdd635c47a2c2d6af26936aeb40a86bee9d3710e
mbickley <mbickley@uvm.edu>
2017-11-17 15:40:03 -0500

    comments for player class

--- Player.cpp
+++ Player.cpp
@@ -3,18 +3,34 @@
 //
 
 #include "Player.h"
-
-//default  constructer
+/*
+ * default constructor
+ */
 Player:: Player(){}
-//construtor sets initial positions (character is null initially)
+//constructor sets initial positions (character is null initially)
+/*
+ * Requires: position (x and y)
+ * Modifies: player object
+ * Effects: player object position
+ */
 Player:: Player(int x, int y){
     setPosition(x,y);
 }
-//sets character as parameter
+/*
+ * sets character as parameter
+ * Requires: Char character
+ * Modifies: Player character field
+ * Effects: player object
+ **/
 void Player:: setCharacter(char characterIn) {
     character = characterIn;
 }
-//for now moves player "up" by SPACE
+/* for now moves player "up" by SPACE
+ * Requires: nothing
+ * Modifies: player position
+ * Effects: player object
+ **/
+
 void Player:: movePiece(){
     setPosition(getX(), getY()-SPACE);
 }
