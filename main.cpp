4bff7646e7b30c36dcc4a14e4e74b846e2a23bb0
Jacob Jonsson <jacobjjonsson@gmail.com>
2017-11-17 22:32:42 -0500

    Merge branch 'master' of github.com:lisa-dion/MCJJ
    # Conflicts:
    #	Player.cpp
    #	Player.h

--- main.cpp
+++ main.cpp
@@ -7,13 +7,11 @@ using namespace std::this_thread;
 using namespace std::chrono;
 
 int main() {
-
     // Create a board
     Board board = Board();
     // Write to a text file and load from a text file
     board.saveGame();
     board.loadGame();
-
 
     return 0;
 }
