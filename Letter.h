806a5abbc5ee5944b0e1ac78a5a9d9c7d27d6883
mbickley <mbickley@uvm.edu>
2017-11-30 17:53:04 -0500

    comments for player class

---
+++ Letter.h
@@ -0,0 +1,32 @@
+//
+// Created by Michael Bickley on 11/30/17.
+//
+
+#ifndef MCJJ_LETTER_H
+#define MCJJ_LETTER_H
+#include "Piece.h"
+
+class Letter {
+public:
+    Letter();
+
+    Letter(Piece p);
+
+    void setLetter(char c);
+
+    char getLetter();
+
+    void setPiece(Piece p);
+
+    Piece getPiece();
+
+    void updatePosition();
+private:
+    Piece p;
+    char letter;
+
+
+};
+
+
+#endif //MCJJ_LETTER_H
