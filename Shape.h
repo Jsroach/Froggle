c43dc10275b642ac5e75a60c018812d55df5f046
cniemczy <cniemczy@uvm.edu>
2017-12-07 17:40:01 -0500

    Created Shape and Rdctangle Classes Built Menu

--- Shape.h
+++ Shape.h
@@ -5,4 +5,52 @@
 #ifndef MCJJ_SHAPE_H
 #define MCJJ_SHAPE_H
 
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+struct color {
+    double red;
+    double green;
+    double blue;
+};
+
+class Shape {
+public:
+    // Constructors
+    Shape();
+    Shape(int xIn, int yIn);
+    Shape(color f, int xIn, int yIn);
+
+    // getters
+    color getFill() const;
+    double getX() const;
+    double getY() const;
+
+    // setters
+    void setFill(double r, double g, double b);
+    void setFill(color f);
+    double setX(int xIn);
+    double setY(int yIn);
+
+    // will add canvas parameter when we learn graphics
+    // pure virtual (class is abstract)
+    virtual void draw() const = 0;
+
+protected:
+    color fill;
+    int x = 0;
+    int y = 0;
+};
+
+class Rectangle1 : public Shape {
+public:
+    // Constructors
+    Rectangle1();
+    Rectangle1(int xIn, int yIn);
+    Rectangle1(color f, int xIn, int yIn);
+
+    void draw() const override;
+
+};
 #endif //MCJJ_SHAPE_H
